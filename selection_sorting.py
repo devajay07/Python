f22a0b8059c9870942b0718f1abb618288ecb9fe
Ajay Yadav <moksh@192.168.1.3>
2022-12-20 23:40:07 +0530

    Learned basic sorting techniques

--- selection_sorting.py
+++ selection_sorting.py
@@ -5,10 +5,12 @@ print('Enter the elements: ')
 for x in range(0, n):
     x = int(input())
     a.append(x)
-for i in range(0, n-1):
-    for j in range(n-1):
-        if (a[j] > a[j+1]):
-            temp = a[j]
-            a[j] = a[j+1]
-            a[j+1] = temp
+print(a)
+for i in range(n-1):
+    minIndex = i
+    for j in range(i+1, n):
+        if a[j] < a[minIndex]:
+            minIndex = j
+
+    a[i], a[minIndex] = a[minIndex], a[i]
 print('Sorted List: ', a)
